1e75b79d2b09692dfbde99ead3b8f366635517c1
benmez1n <ghomri.imad1@gmail.com>
2022-03-19 12:34:24 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+    colors : {
+      "White": "hsl(0, 0%, 100%)",
+      "LightGrey": "hsl(217, 12%, 63%)",
+      "MediumGrey": "hsl(216, 12%, 54%)",
+      "DarkBlue": "hsl(213, 19%, 18%)",
+      "VeryDarkBlue": "hsl(216, 12%, 8%)",
+      "Orange": "hsl(25, 97%, 53%)"
+    },
+  },
+  plugins: [],
+}
